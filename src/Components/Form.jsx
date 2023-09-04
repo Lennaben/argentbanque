cbc296e785ec291f89083bf65bdd336f95638d56
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-09-04 11:40:27 +0200

    MODIF form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react"
-import {log} from "../Login/log.jsx"
+
 
 function Form() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [check, setCheck] = useState(false)
-  const id = {
-    email: email,
-    password: password,
-    check: check,
+  const [message, setMessage ] = useState("")
+
+  let handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      let res = await fetch("http://localhost:3001/api/v1/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+      let resJson = await res.json()
+      if (res.status === 200) {
+        setPassword("")
+        setEmail("")
+        setMessage("User created successfully")
+      } else {
+        setMessage("Some error occured")
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
-  const idUser = JSON.stringify(id);
-   async function handleSubmit(e){
-    e.preventDefault();
-    await log(idUser); 
-  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
         <input
@@ -43,9 +57,7 @@ function Form() {
         <label htmlFor="remember-me">Remember me</label>
       </div>
       {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-      <button className="sign-in-button" onClick={handleSubmit}>
-        Sign In
-      </button>
+      <button className="sign-in-button">Sign In</button>
       {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
       {/* <!-- <button class="sign-in-button">Sign In</button> --> */}
       {/* <!--  --> */}
